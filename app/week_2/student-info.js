59683cdb7ad8c56dc3ec25dde8f379eec4af23e6
Abbas Garad Gure <Abass.GaradGure@edu.sait.ca>
2025-01-28 10:32:24 -0600

    assignment 2

---
+++ app/week_2/student-info.js
@@ -0,0 +1,9 @@
+export default function StudentInfo() {
+    return (
+        <main>
+            <h1>Student Info</h1>
+            <p>Abbas Garad Gure</p>
+            <Link href="https://github.com/new ">Learn More</Link>
+        </main>
+    );
+}
