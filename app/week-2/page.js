cc0dbecdf6087e11453c406aa1fdd42eb2910c5d
Abbas Garad Gure <Abass.GaradGure@edu.sait.ca>
2025-01-28 20:32:35 -0600

    initial commit

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,9 +1,10 @@
-import StudentInfo from "./student-info.js"; // import from student-info.js 
+import StudentInfo from "./student-info";
 
-export default function Page(){
-    return(
-        <main>
-            <h1>Shopping List</h1>
-        </main>
-    );
-}
+export default function Page() {
+  return (
+    <main>
+      <h1>Shopping List</h1>
+      <StudentInfo />
+    </main>
+  );
+} 
--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export default function StudentInfo() {
-    return (
-        <main>
-            <h1>Student Info</h1>
-            <p>Abbas Garad Gure</p>
-            <Link href="https://github.com/new ">Learn More</Link>
-        </main>
-    );
-}
+  return (
+    <main>
+      <h1>Student Info</h1>
+      <p>Abbas Garad Gure</p>
+      <Link href="https://github.com/new">My Github</Link>
+    </main>
+  );
+} 
